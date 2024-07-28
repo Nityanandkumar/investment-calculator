b9f468125d063036ab7940be9d4f50ca50cb4103
nityab1 <nityanandsunny@gmail.com>
2024-07-28 15:57:58 +0530

    added results component and passing data from parent to child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,23 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
 import {type investmentInput } from './investment-input.model';
+import { InvestmentResultsComponent } from './investment-results/investment-results.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent,InvestmentResultsComponent],
 })
 export class AppComponent {
+  resultsData?: {
+    year: number,
+    interest: number,
+    valueEndOfYear: number,
+    annualInvestment: number,
+    totalInterest: number,
+    totalAmountInvested: number,
+  }[];
   onCalculateInvestmentResults( data : investmentInput) {
     const annualData = [];
     const {initialInvestment,duration,annualInvestment,expectedReturn}= data
@@ -31,6 +40,6 @@ export class AppComponent {
       });
     }
 
-   console.log(annualData);
+  this.resultsData=annualData;
   }
 }
