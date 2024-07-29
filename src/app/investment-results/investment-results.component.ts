60727864d2b6220f382e7c88cc844e9827afcc08
nityab1 <nityanandsunny@gmail.com>
2024-07-29 15:51:50 +0530

    using signals in servics

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,5 +1,5 @@
 import { CurrencyPipe } from '@angular/common';
-import { Component,inject,input } from '@angular/core';
+import { Component,inject,input,computed } from '@angular/core';
 import { InvestmentService } from '../investment.service';
 
 @Component({
@@ -11,7 +11,6 @@ import { InvestmentService } from '../investment.service';
 })
 export class InvestmentResultsComponent {
  private investmentService = inject(InvestmentService);
- get results() {
-  return this.investmentService.resultsData;
- }
+  results=computed(()=>this.investmentService.resultsData());
+ 
 }
