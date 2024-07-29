2c6bb379afd9494f27fd19c0eeb173c7522364e8
nityab1 <nityanandsunny@gmail.com>
2024-07-29 15:28:32 +0530

    used signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,signal } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
 import {type investmentInput } from './investment-input.model';
@@ -11,14 +11,14 @@ import { InvestmentResultsComponent } from './investment-results/investment-resu
   imports: [HeaderComponent, UserInputComponent,InvestmentResultsComponent],
 })
 export class AppComponent {
-  resultsData?: {
+  resultsData = signal<{
     year: number,
     interest: number,
     valueEndOfYear: number,
     annualInvestment: number,
     totalInterest: number,
     totalAmountInvested: number,
-  }[];
+  }[] |undefined> (undefined)
   onCalculateInvestmentResults( data : investmentInput) {
     const annualData = [];
     const {initialInvestment,duration,annualInvestment,expectedReturn}= data
@@ -40,6 +40,6 @@ export class AppComponent {
       });
     }
 
-  this.resultsData=annualData;
+  this.resultsData.set(annualData);
   }
 }
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component,output,signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { investmentInput } from '../investment-input.model';
 
@@ -11,17 +11,21 @@ import { investmentInput } from '../investment-input.model';
 })
 export class UserInputComponent {
   //to passing data from child component to parent-component we use eventEmitter means outputdecrator
-  @Output() calculate= new EventEmitter<investmentInput>();
-  enteredInitialInvestment ="0";
-  enteredAnnualInvestment = "0";
-  enteredExpectedReturn ="5";
-  enteredDuration ="10";
+   calculate= output<investmentInput>();
+  enteredInitialInvestment =signal("0");
+  enteredAnnualInvestment = signal("0");
+  enteredExpectedReturn =signal("5");
+  enteredDuration =signal("10");
    onSubmit() {
     this.calculate.emit({
-      initialInvestment: +this.enteredInitialInvestment, //+ used here to convert string type to number
-      annualInvestment: +this.enteredAnnualInvestment,
-      duration: +this.enteredDuration,
-       expectedReturn: +this.enteredExpectedReturn
+      initialInvestment: +this.enteredInitialInvestment(), //+ used here to convert string type to number
+      annualInvestment: +this.enteredAnnualInvestment(),
+      duration: +this.enteredDuration(),
+       expectedReturn: +this.enteredExpectedReturn()
     })
+    this.enteredAnnualInvestment.set('0');
+    this.enteredInitialInvestment.set('0');
+    this.enteredExpectedReturn.set('5');
+    this.enteredDuration.set('10');
    }
 }
